24336f1073528bc032518d384dcf68ac3ec8f682
hamza elbakkouri <112905780+hamzaelbak66@users.noreply.github.com>
2024-04-16 00:58:23 +0100

    setup backend project

---
+++ app/src/main/java/com/app/ouadia/OuadiaApplication.java
@@ -0,0 +1,13 @@
+package com.app.ouadia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuadiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuadiaApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/app/ouadia/OuadiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ouadia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuadiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
